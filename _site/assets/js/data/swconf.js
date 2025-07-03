9987998b5820d773f70dd7d62f8681fa88fbed79
UdayCherri <cherriuday@gmail.com>
2025-07-03 15:55:07 +0530

    removed .nojekyll

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749468025',resources: [
+    cacheName: 'chirpy-1751532332',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
